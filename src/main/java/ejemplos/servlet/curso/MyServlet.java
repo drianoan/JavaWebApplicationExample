b60b8965016ddf6ecdc9bbcf311b0da9e8f03260
Diego Riano <diego_riano@bhd.com.do>
2024-01-26 13:26:13 -0500

    Update MyServlet.java adding form with two inputs and call MyServlet2 Update MyServlet2.java adding print input information
    Signed-off-by: Diego Riano <diego_riano@bhd.com.do>

--- src/main/java/ejemplos/servlet/curso/MyServlet.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet.java
@@ -19,11 +19,23 @@ public class MyServlet extends HttpServlet {
 
 		// send HTML page to client
 		out.println("<html>");
-		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
-		out.println("<body>");
-		out.println("<h1>Ejemplo Servlet</h1>");
+		out.println("<head><title>Ejemplo HTML desde Servlet</title><link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\">");
+		out.println("<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Oswald\">\n");
+		out.println("<link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Open Sans\">");
+		out.println("<style>h1,h2,h3,h4,h5,h6 {font-family: \"Oswald\"}body {font-family: \"Open Sans\"}</style>");
+		out.println("</head><body>");
+		out.println("<h1>Actividad 1 Servlet</h1>");
 		out.println("<p>Este es un ejemplo en el curso de Java para generar HTML desde un Servlet.</p>");
-		out.println("<p><a href=\"/myServlet2\">Vamos al otro Servlet</a></p>");
+
+		out.println("<p>Formulario para enviar información al siguiente servlet</p>");
+		out.println("<form action=\"/myServlet2\">");
+		out.println("<label for=\"fname\">Nombre:</label><br>");
+		out.println("<input type=\"text\" id=\"fname\" name=\"fname\" value=\"Pepe\"><br>");
+		out.println("<label for=\"lname\">Apellido:</label><br>");
+		out.println("<input type=\"text\" id=\"lname\" name=\"lname\" value=\"Perez\"><br><br>");
+		out.println("<input type=\"submit\" value=\"Submit\">");
+		out.println("</form> ");
+
 		out.println("</body></html>");
 	}
 
